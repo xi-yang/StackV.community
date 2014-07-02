61451fa7dca9f1f27793802ca8a8d10fb61faf91
xi-yang <maxyang@umd.edu>
2014-07-02 14:46:44 -0400

    Add newMrsOntModel method. Add Jena model base classes for NML and MRS namespaces.

--- VersaStack-ejb/src/main/java/net/maxgigapop/mrs/common/ModelUtil.java
+++ VersaStack-ejb/src/main/java/net/maxgigapop/mrs/common/ModelUtil.java
@@ -68,6 +68,24 @@ public class ModelUtil {
         return true;
     }
 
+    static public OntModel newMrsOntModel(String topoUri) {
+        OntModel model = ModelFactory.createOntologyModel(OntModelSpec.OWL_MEM_MICRO_RULE_INF);
+        String ttl = String.format("@prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>.\n"
+                + "@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>.\n"
+                + "@prefix xsd: <http://www.w3.org/2001/XMLSchema#>.\n"
+                + "@prefix owl: <http://www.w3.org/2002/07/owl#>.\n"
+                + "@prefix nml: <http://schemas.ogf.org/nml/2013/03/base#>.\n"
+                + "@prefix mrs: <http://schemas.ogf.org/mrs/2013/12/topology#>.\n"
+                + "<%s#ontology> a owl:Ontology;\n"
+                + "    rdfs:label \"An MRS topology description.\n"
+                + "<%s>\n"
+                + "    a   nml:Topology,\n"
+                + "        owl:NamedIndividual.\n", topoUri);
+        //$$ TODO: add ontology schema and namespace handling code
+        model.read(new ByteArrayInputStream(ttl.getBytes()), null, "TURTLE");
+        return model;
+    }
+    
     static public OntModel createUnionOntModel(List<OntModel> modelList) {
         OntModel ontModel = ModelFactory.createOntologyModel(OntModelSpec.OWL_MEM_MICRO_RULE_INF);
         for (OntModel model: modelList) {
@@ -94,6 +112,7 @@ public class ModelUtil {
         }
         return topoModelMap;
     }
+    
     private static List<RDFNode> getTopologyList(Model model) {
         String sparqlString = "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\"\n" +
                 "PREFIX owl: <http://www.w3.org/2002/07/owl#>\"\n" +
@@ -111,14 +130,16 @@ public class ModelUtil {
             	listRes = new ArrayList<RDFNode>();
             listRes.add(node);            
         }
-	return listRes;
+        return listRes;
     }
+
     private static OntModel getTopology(Model model, RDFNode node) {
         OntModel subModel = ModelFactory.createOntologyModel(OntModelSpec.OWL_MEM_MICRO_RULE_INF);
         Set<RDFNode> visited = new HashSet<RDFNode>();
         rdfDFS(node, visited, subModel);
         return subModel;
     }
+    
     private static void rdfDFS( RDFNode node, Set<RDFNode> visited, OntModel ontModel) {
         if ( visited.contains( node )) {
             return;
