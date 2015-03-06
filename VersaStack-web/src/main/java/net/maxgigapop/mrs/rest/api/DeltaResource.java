d7a3824be726d71262eb31f9d4d50bce095bc324
max <max@192.168.206.74>
2015-03-06 17:50:04 -0500

    add "commit" method

--- VersaStack-web/src/main/java/net/maxgigapop/mrs/rest/api/DeltaResource.java
+++ VersaStack-web/src/main/java/net/maxgigapop/mrs/rest/api/DeltaResource.java
@@ -5,7 +5,14 @@
  */
 package net.maxgigapop.mrs.rest.api;
 
+import java.util.concurrent.Future;
 import javax.ejb.EJB;
+import javax.ejb.EJBException;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import net.maxgigapop.mrs.system.HandleSystemCall;
@@ -14,6 +21,7 @@ import net.maxgigapop.mrs.system.HandleSystemCall;
  *
  * @author max
  */
+@Path("delta")
 public class DeltaResource {
     @Context
     private UriInfo context;
@@ -24,6 +32,24 @@ public class DeltaResource {
     public DeltaResource(){
     }
     
+//    @POST
+//    @Consumes({"application/xml","application/json"})
+//    @Path("/{refUUID}/{deltaId}/{action}")
+//    public String propagate(@PathParam("refUUID")String refUUID){
+//        
+//    }
+    
+    @PUT
+    @Consumes({"application/xml","application/json"})
+    @Path("/{refUUID}")
+    public String commit(@PathParam("refUUID")String refUUID){
+        try{
+            systemCallHandler.commitDelta(refUUID);
+        }catch(EJBException e){
+            return e.getMessage();
+        }
+        return "commit successfully";
+    }
     
     
 }
