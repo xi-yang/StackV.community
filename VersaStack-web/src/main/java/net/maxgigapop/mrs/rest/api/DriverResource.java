22d58c4343a3b5ac801b8ea546f2fcfa122d46af
max <max@192.168.206.74>
2015-03-06 16:37:27 -0500

    Add "plug", "pull", and "unplug" method

--- VersaStack-web/src/main/java/net/maxgigapop/mrs/rest/api/DriverResource.java
+++ VersaStack-web/src/main/java/net/maxgigapop/mrs/rest/api/DriverResource.java
@@ -5,9 +5,16 @@
  */
 package net.maxgigapop.mrs.rest.api;
 
+import java.util.HashMap;
 import java.util.Map;
 import javax.ejb.EJB;
+import javax.ejb.EJBException;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
@@ -18,6 +25,7 @@ import net.maxgigapop.mrs.system.HandleSystemCall;
  *
  * @author max
  */
+@Path("driver")
 public class DriverResource {
     @Context
     private UriInfo context;
@@ -34,6 +42,36 @@ public class DriverResource {
         return systemCallHandler.retrieveAllDriverInstanceMap();
     }
     
+    @GET
+    @Produces({"application/xml","application/json"})
+    @Path("/{topoUri}")
+    public DriverInstance pull(@PathParam("topoUri")String topoUri){
+        return systemCallHandler.retrieveDriverInstance(topoUri);
+    }
     
-
+    @DELETE
+    @Path("/{topoUri}")
+    public String unplug(@PathParam("topoUri")String topoUri){
+        try{
+            systemCallHandler.unplugDriverInstance(topoUri);
+        }catch(EJBException e){
+            return e.getMessage();
+        }
+        return "unplug successfully";
+    } 
+    
+    @POST
+    @Consumes({"application/xml","application/json"})
+    @Path("/{topoUri}")
+    public String plug(@PathParam("topoUri")String topoUri){
+        Map<String,String> driverProperties = new HashMap();
+        driverProperties.put("topoUri", topoUri);
+        driverProperties.put("driverEjbPath", "java:module/StubSystemDriver");
+        try{
+            systemCallHandler.plugDriverInstance(driverProperties);
+        }catch(EJBException e){
+            return e.getMessage();
+        }
+        return "plug successfully";
+    }    
 }
