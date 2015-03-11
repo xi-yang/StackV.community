510356e2b4c1cb7a114b1776648171d1113bf531
max <max@192.168.206.74>
2015-03-11 14:17:25 -0400

    add "ApiDeltaBase" object

---
+++ VersaStack-web/src/main/java/net/maxgigapop/mrs/rest/api/model/ApiDeltaBase.java
@@ -0,0 +1,86 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.maxgigapop.mrs.rest.api.model;
+
+import java.util.Date;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author max
+ */
+@XmlRootElement(name="delta")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class ApiDeltaBase {
+    @XmlElement(required=true) 
+    protected String id = "";
+    @XmlElement(required=true) 
+    protected Date creationTime;
+    @XmlElement(required=true)
+    protected String referenceVersion = "";
+    @XmlElement(required=true) 
+    protected String modelReduction = null;
+    @XmlElement(required=true) 
+    protected String modelAddition = null;
+    @XmlElement(required=true) 
+    protected String status = "";
+    
+        public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Date getCreationTime() {
+        return creationTime;
+    }
+
+    public void setCreationTime(Date creationTime) {
+        this.creationTime = creationTime;
+    }
+
+    public String getModelAddition() {
+        return modelAddition;
+    }
+
+    public void setModelAddition(String modelAddition) {
+        this.modelAddition = modelAddition;
+    }
+
+    public String getModelReduction() {
+        return modelReduction;
+    }
+
+    public void setModelReduction(String modelReduction) {
+        this.modelReduction = modelReduction;
+    }
+
+    public String getReferenceVersion() {
+        return referenceVersion;
+    }
+
+    public void setReferenceVersion(String referenceVersion) {
+        this.referenceVersion = referenceVersion;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "net.maxgigapop.versans.model.DeltaBase[ id=" + id + " ]";
+    }
+}
