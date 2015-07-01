18e5c0959d5e45ccb5e3d4cb99c9f48f3e909522
Chen Chen <ccmmbupt@gmail.com>
2015-07-01 11:17:45 -0400

    Bugfix: added missing files

---
+++ VersaStack-web/src/main/webapp/js/versastack/topology/DropDownNode.js
@@ -0,0 +1,60 @@
+"use strict";
+define([
+    "local/versastack/utils"
+    ], function (utils) {
+        var map_=utils.map_;
+    function DropDownNode(name) {
+        this.children =[];
+        this.name=name;
+        var that=this;
+        
+        this.addChild=function(name){
+            var ans = new DropDownNode(name);
+            this.children.push(ans);
+            return ans;
+        };
+        
+        var isExpanded=true;
+        
+        function _getText(){
+            var ans="";
+            if(that.children.length!==0){
+                ans+=isExpanded?"▼":"▶";
+            }
+            ans+=that.name;
+            return ans;
+        }
+        
+        this.getHTML =function(){
+           var ans=document.createElement("div");
+           
+           var content =document.createElement("div");
+           content.className="treeMenu";
+           var text =document.createElement("div");
+           text.innerText=_getText();
+           var childNodes=[];
+           text.onclick = function(){
+               isExpanded = !isExpanded;
+               
+               var vis=isExpanded?"visible":"hidden";
+               map_(childNodes,function(child){
+                   child.style.visibility=vis;
+               });
+               text.innerText=_getText();
+           };
+           content.appendChild(text);
+           
+           map_(this.children, function(child){
+               var toAdd=child.getHTML();
+               childNodes.push(toAdd);
+               content.appendChild(toAdd);
+           });
+           ans.appendChild(content);
+           
+           return ans;
+           
+        };
+    }
+    return DropDownNode;
+});
+
