5c810c210bdf0f21d493652b56c0d1845f84b644
max <max@192.168.206.74>
2015-07-13 14:11:36 -0400

    change the parsing parameters of driver install and uninstall method. Modify the corresponding code body.

--- VersaStack-web/src/main/java/web/beans/serviceBeans.java
+++ VersaStack-web/src/main/java/web/beans/serviceBeans.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -39,58 +40,50 @@ public class serviceBeans {
      * 2 - plugin error.<br />
      * 3 - connection error.<br />
      */
-    public int driverInstall(String driverID, String topoName, String accountID, String accountPW, String awsRegion) {
-        String driver = "";
-        if (driverID.equalsIgnoreCase("stubdriver")) {
-            driver = "<driverInstance><properties><entry><key>topologyUri</key>"
-                    + "<value>urn:ogf:network:rains.maxgigapop.net:2013:topology"
-                    + topoName + "</value></entry><entry><key>driverEjbPath</key>"
-                    + "<value>java:module/StubSystemDriver</value></entry>"
-                    + "<entry><key>stubModelTtl</key>"
-                    + "<value>@prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>.\n"
-                    + "@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>.\n"
-                    + "@prefix xsd: <http://www.w3.org/2001/XMLSchema#>.\n"
-                    + "@prefix owl: <http://www.w3.org/2002/07/owl#>.\n"
-                    + "@prefix nml: <http://schemas.ogf.org/nml/2013/03/base#>.\n"
-                    + "@prefix mrs: <http://schemas.ogf.org/mrs/2013/12/topology#>.\n"
-                    + "<http://www.maxgigapop.net/mrs/2013/topology#> a owl:Ontology;\n"
-                    + "    rdfs:label \"NML-MRS Description of the MAX Research Infrastructure\".\n"
-                    + "<urn:ogf:network:rains.maxgigapop.net:2013:topology>\n"
-                    + "    a   nml:Topology,\n"
-                    + "        owl:NamedIndividual;\n"
-                    + "    nml:hasNode\n"
-                    + "        <urn:ogf:network:rains.maxgigapop.net:2013:clpk-msx-1>,\n"
-                    + "        <urn:ogf:network:rains.maxgigapop.net:2013:clpk-msx-4>."
-                    + "</value></entry></properties></driverInstance>";
-
-        } else if (driverID.equalsIgnoreCase("awsdriver")) {
-            driver = "<driverInstance><properties><entry><key>topologyUri</key>"
-                    + "<value>urn:ogf:network:aws.amazon.com:aws-cloud" + topoName + "</value></entry>"
-                    + "<entry><key>driverEjbPath</key><value>java:module/AwsDriver</value></entry>"
-                    + "<entry><key>aws_access_key_id</key><value>" + accountID + "</value></entry>"
-                    + "<entry><key>aws_secret_access_key</key><value>" + accountPW + "</value></entry>"
-                    + "<entry><key>region</key><value>" + awsRegion + "</value></entry></properties></driverInstance>";
-        } else if (driverID.equalsIgnoreCase("versaNSDriver")) {
-            driver = "<driverInstance><properties><entry><key>topologyUri</key>"
-                    + "<value>urn:ogf:network:sdn.maxgigapop.net:network" + topoName + "</value></entry>"
-                    + "<entry><key>driverEjbPath</key><value>java:module/GenericRESTDriver</value></entry>"
-                    + "<entry><key>subsystemBaseUrl</key><value>http://localhost:8080/VersaNS-0.0.1-SNAPSHOT</value></entry>"
-                    + "</properties></driverInstance>";
-        } else if (driverID.equalsIgnoreCase("openStackDriver")) {
-            driver =  "<driverInstance><properties><entry><key>url</key>"
-                    + "<value>http://max-vlsr2.dragon.maxgigapop.net:35357/v2.0</value></entry>"
-                    + "<entry><key>NATServer</key><value></value></entry>"
-                    + "<entry><key>driverEjbPath</key><value>java:module/OpenStackDriver</value></entry>"
-                    + "<entry><key>username</key><value>" + accountID + "</value></entry><entry>"
-                    + "<key>password</key><value>" + accountPW + "</value></entry><entry><key>topologyUri</key>"
-                    + "<value>urn:ogf:network:openstack.com:openstack-cloud" + topoName + "</value></entry>"
-                    + "<entry><key>tenant</key><value>admin</value></entry></properties></driverInstance>";
-        } else if(driverID.equalsIgnoreCase("StackDriver")){
-            //for VersaStack
-        } else //invalid driverID
-        {
-            return 1;
+    public int driverInstall(Map<String, String> paraMap) {
+        String driver = "<driverInstance><properties>";
+        for(Map.Entry<String, String> entry : paraMap.entrySet()){
+            //if the key indicates what kind of driver it is, put the corresponding ejb path
+            if(entry.getKey().equalsIgnoreCase("driverID")){
+                driver += "<entry><key>driverEjbPath</key>";
+                switch(entry.getValue()){
+                    case "stubdriver":
+                        driver += "<value>java:module/StubSystemDriver</value></entry>";
+                        break;
+                    case "awsdriver":
+                        driver += "<value>java:module/AwsDriver</value></entry>";
+                        break;
+                    case "versaNSDriver":
+                        driver += "<value>java:module/StubSystemDriver</value></entry>";
+                        break;                    
+                    case "openStackDriver":
+                        driver += "<value>java:module/GenericRESTDriver</value></entry>";
+                        break;
+                    case "StackDriver":
+                        // to be filled
+                        break;
+                    default:
+                        break;
+                }
+            }
+            
+            //if it is ttl model, modify the format so that the system can recognize the brackets
+            else if(entry.getKey().equalsIgnoreCase("ttlmodel")){
+                String ttlModel = entry.getValue().replaceAll("<", "&lt;");
+                ttlModel = ttlModel.replaceAll(">", "&gt;");
+                driver += "<entry><key>stubModelTtl</key><value>" + ttlModel +"</value></entry>";
+                
+            }
+            
+            //simply put the key value pair into the string
+            else{
+                driver += "<entry><key>" + entry.getKey() + "</key><value>" 
+                        + entry.getValue() + "</value></entry>";
+            }
         }
+        driver += "</properties></driverInstance>";
+                
+        //push to the system api and get response
         try {
             URL url = new URL(String.format("%s/driver", host));
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
@@ -118,23 +111,7 @@ public class serviceBeans {
      * 2 - unplug error.<br />
      * 3 - connection error.<br />
      */
-    public int driverUninstall(String driverID, String par1, String par2, String par3) {
-        String topoUri = "";
-        if (driverID.equalsIgnoreCase("stubdriver")) {
-            topoUri = "urn:ogf:network:rains.maxgigapop.net:2013:topology";
-        } else if (driverID.equalsIgnoreCase("awsdriver")) {
-            topoUri = "urn:ogf:network:aws.amazon.com:aws-cloud";
-        } else if (driverID.equalsIgnoreCase("versaNSDriver")) {
-            topoUri = "urn:ogf:network:sdn.maxgigapop.net:network";
-        } else if (driverID.equalsIgnoreCase("openStackDriver")) {
-            topoUri = "urn:ogf:network:openstack.com:openstack-cloud";
-        } else if(driverID.equalsIgnoreCase("StackDriver")){
-            //for VersaStack
-        } else //invalid driverID
-        {
-            return 1;
-        }
-
+    public int driverUninstall(String topoUri) {
         try {
             URL url = new URL(String.format("%s/driver/%s", host, topoUri));
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
