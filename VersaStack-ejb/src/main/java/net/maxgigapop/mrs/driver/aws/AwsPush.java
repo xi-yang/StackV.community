2d7d19ab6d0f5b2be4073f5c217e623a26b0fa30
Miguel Uzcategui <muzcategui1106@gmail.com>
2015-11-12 13:02:09 -0500

    Fixed typo in AWS push

--- VersaStack-ejb/src/main/java/net/maxgigapop/mrs/driver/aws/AwsPush.java
+++ VersaStack-ejb/src/main/java/net/maxgigapop/mrs/driver/aws/AwsPush.java
@@ -294,7 +294,7 @@ public class AwsPush {
                         ec2.deleteVpc(vpcRequest);
                     } catch (AmazonServiceException e) {
                         try {
-                            Thread.sleep(1000);                 //1000 milliseconds is one second.
+                            Thread.sleep(10000);                 //1000 milliseconds is one second.
                         } catch (InterruptedException ex) {
                             Thread.currentThread().interrupt();
                         }
