84226f71b5459ed3978e38ca50fbf719f9ed931b
aheard <aheard@192.168.206.131>
2016-06-23 12:41:27 -0400

    Fixes bug of float dropped bullets aside relationships in display panel

--- VersaStack-web/src/main/webapp/js/versastack/topology/DropDownNode.js
+++ VersaStack-web/src/main/webapp/js/versastack/topology/DropDownNode.js
@@ -63,7 +63,7 @@ define([
             } else if (that.name.substring(0,3) === "urn") {
                 var bullet = document.createElement("span");
                 bullet.innerHTML = "•";
-                bullet.style.float = "left";
+                if (that.type !== "Relationship") bullet.style.float = "left";
                 line.appendChild(bullet);
             }
             
