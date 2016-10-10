3f753f912f92be0fa3d5146aa4613cce46fc00ad
aheard <aheard@192.168.206.131>
2016-10-10 09:33:13 -0400

    Catches Policy Data JSON formatting exception

--- VersaStack-web/src/main/webapp/js/versastack/utils.js
+++ VersaStack-web/src/main/webapp/js/versastack/utils.js
@@ -168,16 +168,28 @@ define([], function () {
         var parsedData = "";
         var parseString = "";
         if (start > 0 && end > 0) {
-            if (endComma !== -1)
-                newstr = data.slice(0, start-1) + '\"' + data.slice(start,end+1) + '\"' + data.slice(endComma+1); 
-            else 
-                newstr = data.slice(0, start-1) + '\"' + data.slice(start,end+1) + '\"' + data.slice(end+1); 
+            if (endComma !== -1) {
+                var commaAfter = data.indexOf(",", end);
+                var dqAfter = data.indexOf("\"", end);
 
-            parsedData = JSON.parse(newstr);
-            parseString = JSON.stringify(parsedData, null, 4);
-            start = parseString.indexOf("%$")-1;
-            end = parseString.indexOf("%", start+2)-1;
-            parseString =  parseString.slice(0, start-1)  + parseString.slice(start+1 ,end+2) + parseString.slice(end+3);
+                if ((commaAfter === -1 || commaAfter > dqAfter) && dqAfter !== -1) {
+                  newstr = data.slice(0, start-1) + '\"' + data.slice(start,end+1) + '\",' + data.slice(endComma+1); 
+                } else {
+                  newstr = data.slice(0, start-1) + '\"' + data.slice(start,end+1) + '\"' + data.slice(endComma+1); 
+                }
+            }else {
+                newstr = data.slice(0, start-1) + '\"' + data.slice(start,end+1) + '\"' + data.slice(end+1); 
+            }
+            
+            try {
+                parsedData = JSON.parse(newstr);
+                parseString = JSON.stringify(parsedData, null, 4);
+                start = parseString.indexOf("%$")-1;
+                end = parseString.indexOf("%", start+2)-1;
+                parseString =  parseString.slice(0, start-1)  + parseString.slice(start+1 ,end+2) + parseString.slice(end+3);
+            } catch(err) {
+                return data;
+            }
         } else {
             parsedData = JSON.parse(data);
             parseString = JSON.stringify(parsedData, null, 4);
