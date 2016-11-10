a1de841da2428a023cf8055e4f36d27aa8249f88
R-Jimenez <rikenavadur@192.168.206.78>
2016-11-10 09:26:42 -0500

    - Interceptor workflow fix

--- VersaStack-web/src/main/java/net/maxgigapop/mrs/rest/api/SecurityInterceptor.java
+++ VersaStack-web/src/main/java/net/maxgigapop/mrs/rest/api/SecurityInterceptor.java
@@ -22,13 +22,21 @@
  */
 package net.maxgigapop.mrs.rest.api;
 
-import javax.ws.rs.WebApplicationException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.ws.rs.ext.Provider;
 import org.jboss.resteasy.annotations.interception.ServerInterceptor;
 import org.jboss.resteasy.core.Headers;
 import org.jboss.resteasy.core.ResourceMethodInvoker;
 import org.jboss.resteasy.core.ServerResponse;
-import org.jboss.resteasy.spi.Failure;
 import org.jboss.resteasy.spi.HttpRequest;
 import org.jboss.resteasy.spi.interception.PreProcessInterceptor;
 import org.keycloak.KeycloakSecurityContext;
@@ -40,16 +48,50 @@ public class SecurityInterceptor implements PreProcessInterceptor {
 
     private static final ServerResponse ACCESS_DENIED = new ServerResponse("Access denied for this resource.\n", 401, new Headers<Object>());
     private static final ServerResponse SERVER_ERROR = new ServerResponse("INTERNAL SERVER ERROR\n", 500, new Headers<Object>());
+    private final String front_db_user = "front_view";
+    private final String front_db_pass = "frontuser";
 
     @Override
-    public ServerResponse preProcess(HttpRequest request, ResourceMethodInvoker method)
-            throws Failure, WebApplicationException {
-        ServerResponse response;
+    public ServerResponse preProcess(HttpRequest request, ResourceMethodInvoker method) {
+        List<String> supplierNames = Arrays.asList("loadEditor", "loadWizard");
+        String methodName = method.getMethod().getName();
+        if (supplierNames.contains(methodName)) {
+            return null;
+        }
 
         KeycloakSecurityContext securityContext = (KeycloakSecurityContext) request.getAttribute(KeycloakSecurityContext.class.getName());
         AccessToken accessToken = securityContext.getToken();
-        
+        String subject = accessToken.getSubject();
 
+        Properties front_connectionProps = new Properties();
+        front_connectionProps.put("user", front_db_user);
+        front_connectionProps.put("password", front_db_pass);
+        Connection front_conn;
+        try {
+            front_conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/frontend",
+                    front_connectionProps);
+
+            PreparedStatement prep = front_conn.prepareStatement("SELECT * FROM acl WHERE subject=? AND object=?");
+            prep.setString(1, subject);
+            prep.setString(2, methodName);
+            ResultSet rs1 = prep.executeQuery();
+            if (rs1.next()) {
+                return null;
+            } else {
+                return ACCESS_DENIED;
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(SecurityInterceptor.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        /*
+        switch (methodName) {
+            case "loadWizard":
+                break;
+            
+            default:
+                break;
+        }*/
         if (false) {
             return ACCESS_DENIED;
         }
