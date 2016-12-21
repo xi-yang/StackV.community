72560c3e8800611f0132fdc5d7ec85b7c0b0d14f
R-Jimenez <rikenavadur@panopticon.home>
2016-12-21 12:14:24 -0500

    - git fix 2

--- StackV-web/target/StackV-web-1.0-SNAPSHOT/tools/testing/driverinstall.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-curl -X POST -d @driver-ops.xml --header "Content-Type:application/xml" http://127.0.0.1:8080/StackV-web/restapi/driver;
-curl -X POST -d @driver-vns.xml --header "Content-Type:application/xml" http://127.0.0.1:8080/StackV-web/restapi/driver;
-curl -X POST -d @driver-stub-wan.xml --header "Content-Type:application/xml" http://127.0.0.1:8080/StackV-web/restapi/driver;
-curl -X POST -d @driver-stub-fake.xml --header "Content-Type:application/xml" http://127.0.0.1:8080/StackV-web/restapi/driver;
-curl -X POST -d @driver-stack-anl.xml --header "Content-Type:application/xml" http://127.0.0.1:8080/StackV-web/restapi/driver;
