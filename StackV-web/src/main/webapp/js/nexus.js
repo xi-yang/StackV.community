8a8365adb0eed1deeca3c39d825baea34a4ad7e9
R-Jimenez <ajimen93@umd.edu>
2017-05-17 13:16:09 -0400

    - Fix for details rows

--- StackV-web/src/main/webapp/js/nexus.js
+++ StackV-web/src/main/webapp/js/nexus.js
@@ -1119,13 +1119,13 @@ function loadDataTable(apiUrl) {
 function formatChild(d) {
     // `d` is the original data object for the row
     var retString = '<table cellpadding="5" cellspacing="0" border="0">';
-    if (!d.message === "{}") {
+    if (!(d.message === "{}")) {
         retString += '<tr>' +
             '<td style="width:10%">Message:</td>' +
             '<td style="white-space: normal">' + d.message + '</td>' +
             '</tr>';
     }
-    if (!d.message === "{}") {
+    if (!(d.message === "{}")) {
         retString += '<tr>' +
             '<td>Exception:</td>' +
             '<td>' + d.exception + '</td>' +
