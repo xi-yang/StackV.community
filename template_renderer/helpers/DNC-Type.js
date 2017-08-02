6deb0e313930b68ea190487ab48f28e7943ad5c8
Jason Chulock <jchulock@gmail.com>
2017-08-02 17:27:05 -0400

    #619 added Multi-Path P2P VLAN

--- template_renderer/helpers/DNC-Type.js
+++ template_renderer/helpers/DNC-Type.js
@@ -1,5 +1,7 @@
 (function(type) {
     switch (type) {
+        case 'Multi-Path P2P VLAN':
+            return 'MCE_MPVlanConnection';
         case 'Multi-Point VLAN Bridge':
             return 'MCE_MultiPointVlanBridge';
     }
