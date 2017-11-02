e152d6a83e5f64a50c0995f9dde58ec1fe4b5c48
R-Jimenez <ajimen93@umd.edu>
2017-11-02 10:07:06 -0400

    #660 - Gateway helper fix

--- StackV-web/src/main/java/templateengine/helpers/vcn/ifVCNGateways.java
+++ StackV-web/src/main/java/templateengine/helpers/vcn/ifVCNGateways.java
@@ -9,11 +9,13 @@ public class ifVCNGateways implements Helper {
 
     @Override
     public String apply(ArrayList<Object> obj) {
-        JSONArray arr = (JSONArray) obj.get(0);
-        for (Object eleObj : arr) {
-            JSONObject ele = (JSONObject) eleObj;
-            if (ele.get("type") != null && ele.get("type").equals("AWS Direct Connect")) {
-                return "true";
+        if (obj.get(0) instanceof JSONArray) {
+            JSONArray arr = (JSONArray) obj.get(0);
+            for (Object eleObj : arr) {
+                JSONObject ele = (JSONObject) eleObj;
+                if (ele.get("type") != null && ele.get("type").equals("AWS Direct Connect")) {
+                    return "true";
+                }
             }
         }
         return "false";
