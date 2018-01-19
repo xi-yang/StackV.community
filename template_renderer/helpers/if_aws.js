e41271a9f2102d4cdc3310395e197c2802ca1139
Xi Yang <maxyang@umd.edu>
2018-01-18 21:09:57 -0500

    cleanup for release

--- template_renderer/helpers/if_aws.js
+++
@@ -1,8 +0,0 @@
-(function(root, opts) {
-    if (root.options.includes('aws-form') || // array
-            root.parent.includes('amazon')) {   // string
-        return opts.fn(this);
-    }
-    
-    return opts.inverse(this);
-});
--- template_renderer/helpers/if_ops.js
+++
@@ -1,8 +0,0 @@
-(function(root, opts) {
-    if (root.options.includes('openstack-form') ||   // array
-            root.parent.includes('openstack')) {        // string
-        return opts.fn(this);
-    } else {
-        return opts.inverse(this);
-    }
-});
