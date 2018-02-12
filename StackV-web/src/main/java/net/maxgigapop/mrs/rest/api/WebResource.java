083c2b2165c32b7d44c3878eac13e9e8440d170a
Sai Arvind <sai.arvindg@gmail.com>
2018-02-12 14:04:22 -0500

    Changed delete all policies endpoint to accept URL parameter instead of data parameter (#702)

--- StackV-web/src/main/java/net/maxgigapop/mrs/rest/api/WebResource.java
+++ StackV-web/src/main/java/net/maxgigapop/mrs/rest/api/WebResource.java
@@ -553,37 +553,11 @@ public class WebResource {
     }
     
     @DELETE
-    @Path("/acl/ipa/servicepolicies")
-    @Consumes("application/x-www-form-urlencoded")
+    @Path("/acl/ipa/servicepolicies/{serviceUUID}")    
     @Produces("application/json")
     @RolesAllowed("ACL")
-    public String ipaDeleteAllPoliciesForService(String data) throws UnsupportedEncodingException {  
-        JSONObject result = new JSONObject();
-        
-        //create a new Mapping of keys to values in the parameters
-        final Map<String, List<String>> postData_params = new HashMap<String, List<String>>();
-        final String[] params = data.split("&"); // each pair will be separated by &
-        
-        // go thru each pair of variables and extract the key and value
-        for (String pair : params) {
-            final int index = pair.indexOf("="); // a pairing is indicated by =
-            
-            // decode the key using UTF-8 if there is a =, otherwise return the pair
-            final String key = index > 0 ? URLDecoder.decode(pair.substring(0, index), "UTF-8") : pair;
-            
-            //add the key to the map if it does not exist already
-            if (!postData_params.containsKey(key)) {
-                postData_params.put(key, new LinkedList<String>());
-            }
-            
-            // decode the value using UTF-8 if there is a =, otherwise return null
-            final String val = index > 0 && pair.length() > index + 1 ? URLDecoder.decode(pair.substring(index + 1), "UTF-8") : null; 
-            postData_params.get(key).add(val); //add the value to the key
-        }
-        
-        
-        //at this point all the parameters are in postData_params
-        String uuid = postData_params.get("serviceUUID").get(0); // there should only be one serviceUUID
+    public String ipaDeleteAllPoliciesForService(@PathParam("serviceUUID") String uuid, String data) throws UnsupportedEncodingException {  
+        JSONObject result = new JSONObject();                                    
         
         // formatting all the groups and rules names
         String loginUg = "ug-login-" + uuid;
