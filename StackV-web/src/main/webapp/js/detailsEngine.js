f37371ffa4ccdead50fbba1124268196c15116b1
R-Jimenez <ajimen93@umd.edu>
2018-02-14 15:09:26 -0500

    Verification fixes

--- StackV-web/src/main/webapp/js/detailsEngine.js
+++ StackV-web/src/main/webapp/js/detailsEngine.js
@@ -70,7 +70,7 @@ function renderDetails() {
             instruction = instruction.replace("{{" + str + "}}", result);
         }
     }
-    if (verificationHasDrone) {
+    if (verificationHasDrone && verificationElapsed) {
         instruction += " (Verification elapsed time: " + verificationElapsed + ")";
     }
     $instruction.html(instruction);
@@ -80,7 +80,7 @@ function renderDetails() {
     for (var i in buttons) {
         var button = buttons[i];
         if (button === "verify" && verificationHasDrone) {
-            ;
+            $("#unverify").removeClass("hide");
         } else if (button === "cancel" && superState === "CANCEL") {
             $("#reinstate").removeClass("hide");
         } else if (button === "force_cancel" && superState === "CANCEL") {
@@ -240,8 +240,10 @@ function attachListeners() {
                             }
                         });
                     } else {
-                        swal("Loading!", {
+                        swal({
                             buttons: false,
+                            icon: "success",
+                            closeOnEsc: false,
                             timer: 3000
                         });
 
