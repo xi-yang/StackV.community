c725ccac9b3b510afe8998b3cf40986a5eacc86a
R-Jimenez <ajimen93@umd.edu>
2018-03-29 09:22:31 -0400

    #738 - Manifest fixes

--- StackV-web/src/main/webapp/js/manifest.js
+++ StackV-web/src/main/webapp/js/manifest.js
@@ -103,11 +103,11 @@ function parseMap(map, container, container_type, base) {
                 var n3 = $("<ul class=\"manifest-list\" style=\"padding-left:.5em;\"></ul>");
                 n1.append(n3);
                 for (var i in map[key]) {
-                    if ((typeof map[key[i]]) === "string") {
-                        string += "<li><b> " + i + "</b>: " + map[key][i] + "</li>";
+                    if ((typeof map[key]) === "string") {
+                        string += "<li><b> " + map[key][i] + "</b>: " + map[key][i] + "</li>";
                         container.append(string);
                         string = "";
-                    } else if (map[key][i].constructor === Array) {
+                    } else if (map[key].constructor === Array) {
                         parseMap(map[key][i], n3, "list", base);
                     }
                 }
