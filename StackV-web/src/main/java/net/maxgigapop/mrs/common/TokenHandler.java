074d0a64e2a54181a1bb225402d6180aaf6b5377
R-Jimenez <ajimen93@umd.edu>
2018-04-26 11:24:05 -0400

    #765 - Profile usage fix

--- StackV-web/src/main/java/net/maxgigapop/mrs/common/TokenHandler.java
+++ StackV-web/src/main/java/net/maxgigapop/mrs/common/TokenHandler.java
@@ -57,7 +57,7 @@ public class TokenHandler {
         if (refresh != null) {
             refreshToken = refresh;
             accessToken = refreshTokenSub(0);
-        }                
+        }                                
     }
 
     public void refreshToken() {
