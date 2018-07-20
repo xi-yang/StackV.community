df13509346576539728249215287a1e8853a26dc
root <xyang@maxgigapop.net>
2018-07-20 18:39:45 +0000

    driverInstance refresh in commitPush

--- StackV-ejb/src/main/java/net/maxgigapop/mrs/driver/aws/AwsDriver.java
+++ StackV-ejb/src/main/java/net/maxgigapop/mrs/driver/aws/AwsDriver.java
@@ -106,7 +106,7 @@ public class AwsDriver implements IHandleDriverSystemCall {
         if (driverInstance == null) {
             throw logger.error_throwing(method, "DriverInstance == null");
         }
-        //driverInstance = DriverInstancePersistenceManager.findById(driverInstance.getId());
+        driverInstance = DriverInstancePersistenceManager.findById(driverInstance.getId());
         String requests = aDelta.getCommand("requests");
         if (requests == null) {
             throw logger.error_throwing(method, "requests == null - something wrong with requests from propagate.");
