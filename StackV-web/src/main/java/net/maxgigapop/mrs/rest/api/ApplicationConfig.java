e02ff8af1ff976ed8ec134cd20a505c0f9a0286f
Xi Yang <maxyang@umd.edu>
2019-01-29 11:49:41 -0500

    REST API class mappers change

--- StackV-web/src/main/java/net/maxgigapop/mrs/rest/api/ApplicationConfig.java
+++ StackV-web/src/main/java/net/maxgigapop/mrs/rest/api/ApplicationConfig.java
@@ -46,9 +46,6 @@ public class ApplicationConfig extends Application {
     }
 
     private void addRestResourceClasses(Set<Class<?>> resources) {
-        resources.add(io.swagger.jaxrs.listing.AcceptHeaderApiListingResource.class);
-        resources.add(io.swagger.jaxrs.listing.ApiListingResource.class);
-        resources.add(io.swagger.jaxrs.listing.SwaggerSerializers.class);
         resources.add(net.maxgigapop.mrs.rest.api.CORSResponseFilter.class);
         resources.add(net.maxgigapop.mrs.rest.api.ConfigResource.class);
         resources.add(net.maxgigapop.mrs.rest.api.DeltaResource.class);
@@ -63,25 +60,6 @@ public class ApplicationConfig extends Application {
         resources.add(net.maxgigapop.mrs.rest.api.exception.EJBExceptionMapper.class);
         resources.add(net.maxgigapop.mrs.rest.api.exception.IOExceptionMapper.class);
         resources.add(net.maxgigapop.mrs.rest.api.exception.ParseExceptionMapper.class);
-        resources.add(net.maxgigapop.mrs.rest.api.exception.SQLExceptionMapper.class);       
-        resources.add(org.jboss.resteasy.client.exception.mapper.ApacheHttpClient4ExceptionMapper.class);
-        resources.add(org.jboss.resteasy.core.AcceptHeaderByFileSuffixFilter.class);                
-        resources.add(org.jboss.resteasy.plugins.interceptors.encoding.AcceptEncodingGZIPFilter.class);
-        resources.add(org.jboss.resteasy.plugins.interceptors.encoding.AcceptEncodingGZIPInterceptor.class);
-        resources.add(org.jboss.resteasy.plugins.interceptors.encoding.GZIPDecodingInterceptor.class);
-        resources.add(org.jboss.resteasy.plugins.interceptors.encoding.GZIPEncodingInterceptor.class);
-        resources.add(org.jboss.resteasy.plugins.providers.DataSourceProvider.class);
-        resources.add(org.jboss.resteasy.plugins.providers.DefaultNumberWriter.class);
-        resources.add(org.jboss.resteasy.plugins.providers.DefaultTextPlain.class);
-        resources.add(org.jboss.resteasy.plugins.providers.DocumentProvider.class);
-        resources.add(org.jboss.resteasy.plugins.providers.FileProvider.class);
-        resources.add(org.jboss.resteasy.plugins.providers.FormUrlEncodedProvider.class);
-        resources.add(org.jboss.resteasy.plugins.providers.IIOImageProvider.class);
-        resources.add(org.jboss.resteasy.plugins.providers.InputStreamProvider.class);
-        resources.add(org.jboss.resteasy.plugins.providers.JaxrsFormProvider.class);
-        resources.add(org.jboss.resteasy.plugins.providers.ReaderProvider.class);
-        resources.add(org.jboss.resteasy.plugins.providers.SerializableProvider.class);
-        resources.add(org.jboss.resteasy.plugins.providers.SourceProvider.class);
-        resources.add(org.jboss.resteasy.plugins.providers.StringTextStar.class);
+        resources.add(net.maxgigapop.mrs.rest.api.exception.SQLExceptionMapper.class);
     }
 }
