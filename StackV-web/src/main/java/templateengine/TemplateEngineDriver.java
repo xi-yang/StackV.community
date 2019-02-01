7ae7804d16a47f07673377ad7fd3f97294efd607
R-Jimenez <ajimen93@umd.edu>
2019-02-01 16:15:17 -0500

    AWS Globus template changes #744

--- StackV-web/src/main/java/templateengine/TemplateEngineDriver.java
+++ StackV-web/src/main/java/templateengine/TemplateEngineDriver.java
@@ -1,10 +1,13 @@
 package templateengine;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
@@ -14,19 +17,17 @@ public class TemplateEngineDriver {
     public static void main(String[] args) throws IOException {
         try {
             TemplateEngine template = new TemplateEngine();
-            
-            String content = new String(Files.readAllBytes(Paths.get("/Users/rikenavadur/NetBeansProjects/TemplateEngine/src/templateengine/input_dnc")));
-            //String content = new String(Files.readAllBytes(Paths.get("/Users/rikenavadur/NetBeansProjects/TemplateEngine/src/templateengine/input_0")));
-            //String content = new String(Files.readAllBytes(Paths.get("/Users/rikenavadur/NetBeansProjects/TemplateEngine/src/templateengine/input_nic")));
-            //String content = new String(Files.readAllBytes(Paths.get("/Users/rikenavadur/NetBeansProjects/TemplateEngine/src/templateengine/input_aws")));
-            //String content = new String(Files.readAllBytes(Paths.get("/Users/rikenavadur/NetBeansProjects/TemplateEngine/src/templateengine/input_ops")));
-            //String content = new String(Files.readAllBytes(Paths.get("/Users/rikenavadur/NetBeansProjects/TemplateEngine/src/templateengine/input_ahc")));
-            //String content = new String(Files.readAllBytes(Paths.get("/Users/rikenavadur/NetBeansProjects/TemplateEngine/src/templateengine/input_ec")));
+            String content = new String(
+                    Files.readAllBytes(Paths.get("StackV-web/src/main/java/templateengine/input_1")));
             JSONParser parser = new JSONParser();
             JSONObject input = (JSONObject) parser.parse(content);
-            
-            String retString = template.apply(input);            
+
+            String retString = template.apply(input);
             System.out.println("\n\n\nResult:\n" + retString);
+            BufferedWriter writer = new BufferedWriter(
+                    new FileWriter("StackV-web/src/main/java/templateengine/output"));
+            writer.write(retString);
+            writer.close();
         } catch (ParseException ex) {
             Logger.getLogger(TemplateEngineDriver.class.getName()).log(Level.SEVERE, null, ex);
         }
